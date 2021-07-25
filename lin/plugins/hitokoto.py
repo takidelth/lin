e3a4bc0cafefcbfa3df98ca7bdb0de8e10cb55e3
Taki <wh1037447217@outlook.com>
2021-07-25 15:34:25 +0800

    ✨finished hitokoto plugin✨

--- lin/plugins/hitokoto.py
+++ lin/plugins/hitokoto.py
@@ -1,6 +1,29 @@
+from random import choice
+
+from nonebot.adapters.cqhttp import Bot, MessageEvent
+
+from lin.service import on_command
+from lin.utils.requests import get_json
+
 URL = [
-    "https://cdn.jsdelivr.net/gh/hitokoto-osc/sentences-bundle@1.0.84/sentences/a.json",
-    "https://cdn.jsdelivr.net/gh/hitokoto-osc/sentences-bundle@1.0.84/sentences/b.json",
-    "https://cdn.jsdelivr.net/gh/hitokoto-osc/sentences-bundle@1.0.84/sentences/c.json",
+    "https://cdn.jsdelivr.net/gh/hitokoto-osc/sentences-bundle@latest/sentences/a.json",
+    "https://cdn.jsdelivr.net/gh/hitokoto-osc/sentences-bundle@latest/sentences/b.json",
+    "https://cdn.jsdelivr.net/gh/hitokoto-osc/sentences-bundle@latest/sentences/c.json",
 ]
 
+__doc__ = """
+一言
+使用:
+  hitokoto
+  一言
+"""
+hitokoto = on_command(
+    cmd="一言", aliases={"hitokoto"}, docs=__doc__
+)
+
+
+@hitokoto.handle()
+async def _hitokoto(bot: Bot, event: MessageEvent) -> None:
+    data = get_json(choice(URL))
+    repo = data.get("hitokoto")
+    hitokoto.finish(repo)
